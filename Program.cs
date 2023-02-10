2ac15ffc36a9a0970dc9b39add988189d63a3210
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-10 14:13:39 +0530

    Greeting message

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LineComparisonProblem
+{
+    public class program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+        }
+    }
+}
+
